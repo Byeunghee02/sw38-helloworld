2e607d63b06c752af2e093c24b59f100641bc779
unknown <choibh6258@hanyang.ac.kr>
2022-05-19 18:39:11 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { getStorage, ref } from "firebase/storage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const storage = getStorage();
+const gsReference = ref(storage,'gs://hello-world-437fa.appspot.com/muggle.png');
+
+function App () {
+    return(
+        <div className="App">
+            <img src="{gsReference}" alt="muggle"></img>
+        </div>
+    );
 }
 
 export default App;
